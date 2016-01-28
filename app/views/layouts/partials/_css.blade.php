a51cf6668cc87b804c0d4517a16c37313d5d8691
Dave Richer <dave@develops.io>
2016-01-27 23:19:38 -0500

    Updated font-awesome cdn, previous one seems to have vanished

--- app/views/layouts/partials/_css.blade.php
+++ app/views/layouts/partials/_css.blade.php
@@ -11,7 +11,7 @@
 {{-- Bootstrap 3 Minified CSS--}}
 {{ HTML::style('css/vendor/bootstrap.min.css')}}
 {{-- Font Awesome CDN --}}
-{{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css')}}
+{{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css')}}
 {{-- Bootstrap 3 Theme CSS --}}
 {{ HTML::style('css/vendor/bootstrap-theme.min.css')}}
 {{-- Site Specific  CSS --}}
